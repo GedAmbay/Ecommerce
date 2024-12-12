f9bc89ef735c34212e05c9a1fb029d44bbf76191
Ged <mboyambay04@gmail.com>
2024-12-12 14:37:02 +0800

    built in api test

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto px-4">
+    <header className="my-4">
+      <h1 className="text-3xl font-bold text-center">Welcome to Our Shop</h1>
+    </header>
+  </div>
+  );
+}
